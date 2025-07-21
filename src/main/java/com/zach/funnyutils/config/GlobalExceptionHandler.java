75d4260bb97393df54a81585d0ebc4e9ca333d15
zachshen777 <15052379214@163.com>
2025-07-21 17:47:21 +0800

    配置修改

---
+++ src/main/java/com/zach/funnyutils/config/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.zach.funnyutils.config;
+
+import com.zach.funnyutils.exception.RateLimitException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RateLimitException.class)
+    public ResponseEntity<Map<String, Object>> handleRateLimitException(RateLimitException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("retryAfter", ex.getRetryAfter());
+        return new ResponseEntity<>(response, HttpStatus.TOO_MANY_REQUESTS);
+    }
+}
